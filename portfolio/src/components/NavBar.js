35d5f1b2f2c4dd07035ceedfd8cf53c687e31452
Melissa Hookey <melissah315@gmail.com>
2023-04-25 19:19:15 -0500

    resume and icons

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -1,14 +1,22 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
-
+import { Envelope, Linkedin } from "react-bootstrap-icons";
+import Pdf from "../documents/Resume.pdf";
 export const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Navbar.Brand href="#home">Melissa Hookey</Navbar.Brand>
+        <Nav className="justify-content-end">
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href={Pdf} target="_blank">
+            Resume
+          </Nav.Link>
+          <Nav.Link href="#email">
+            <Envelope></Envelope>
+          </Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/melissahookey/">
+            <Linkedin></Linkedin>
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
